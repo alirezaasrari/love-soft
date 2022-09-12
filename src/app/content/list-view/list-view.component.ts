474ec4e7ab8e6585adb8aabd153cc9e0673ea1f5
alireza asrari <alirezaasrari1990@gmail.com>
2022-09-12 14:21:27 +0430

    update admin panel styles

--- src/app/content/list-view/list-view.component.ts
+++ src/app/content/list-view/list-view.component.ts
@@ -3,24 +3,22 @@ import {Component,Input} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import { MediaObserver, MediaChange } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
+import { RegisterService } from 'src/app/services/register.service';
 
 export interface PeriodicElement {
   name: string;
-  position: string;
-  weight: string;
-  symbol: string;
-  symbo: string;
-  symbl: string
+  usercode: string;
+  roll: string;
+  dateofjoin: string;
+  email: string;
+  lastactivity: string
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
-  {position: 'چیستا محمدی', name: '100256', weight: 'حسابدار', symbol: '1400/12/20', symbo: 'd@gmail.com', symbl: 'امروز-10:42'},
+  {name: 'چیستا محمدی', usercode: '100256', roll: 'حسابدار', dateofjoin: '1400/12/20', email: 'd@gmail.com', lastactivity: 'امروز-10:42'},
+  {name: 'چیستا محمدی', usercode: '100256', roll: 'حسابدار', dateofjoin: '1400/12/20', email: 'd@gmail.com', lastactivity: 'امروز-10:42'},
+  {name: 'چیستا محمدی', usercode: '100256', roll: 'حسابدار', dateofjoin: '1400/12/20', email: 'd@gmail.com', lastactivity: 'امروز-10:42'},
+  {name: 'چیستا محمدی', usercode: '100256', roll: 'حسابدار', dateofjoin: '1400/12/20', email: 'd@gmail.com', lastactivity: 'امروز-10:42'},
 ];
 
 /**
@@ -32,7 +30,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./list-view.component.css']
 })
 export class listViewComponent {
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol', 'symbo', 'symbl'];
+  displayedColumns: string[] = ['select', 'name', 'usercode', 'roll',
+   'dateofjoin', 'email', 'lastactivity'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
 
@@ -58,12 +57,12 @@ export class listViewComponent {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.name + 1}`;
   }
   mediaSub:Subscription;
   deviceXs: boolean;
   deviceLg: boolean;
-  constructor(public mediaObserver:MediaObserver){}
+  constructor(public mediaObserver:MediaObserver, private service: RegisterService){}
   ngOnInit(): void {
     this.mediaSub = this.mediaObserver.asObservable().
     subscribe((change: MediaChange[]) => {
